52816a362e92dc2772689b01cdec2fbc1fd0cae9
jula1717 <82888111+jula1717@users.noreply.github.com>
2023-11-01 14:48:41 +0100

    layouts and data class, navGraph

---
+++ app/src/main/java/com/example/tasks2/Task.kt
@@ -0,0 +1,12 @@
+package com.example.tasks2
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import java.text.DateFormat
+@Entity(tableName = "tasks_table")
+@Parcelize
+data class Task (val name:String,val important:Boolean,val done:Boolean, val date:Long = System.currentTimeMillis(),@PrimaryKey (autoGenerate = true) val id: Int = 0) : Parcelable{
+    val dateFormated = DateFormat.getDateInstance().format(date)
+}
